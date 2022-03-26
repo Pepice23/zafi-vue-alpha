2a15472c104f74fb66569b1232db4d66ed1ef53a
Pepice23 <mojo12984@gmail.com>
2022-03-26 21:10:14 +0100

    added race

--- src/stores/newCharacterStore.js
+++ src/stores/newCharacterStore.js
@@ -4,11 +4,15 @@ export const useNewCharacterStore = defineStore({
   id: "newCharacter",
   state: () => ({
     gender: "",
+    race: {},
   }),
   getters: {},
   actions: {
     setGender(gender) {
       this.gender = gender;
+    },
+    setRace(race) {
+      this.race = race;
     },
   },
 });
