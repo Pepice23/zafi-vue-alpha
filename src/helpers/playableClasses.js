4777ee943e75a256c57d72d8727ae00ca948de1b
Pepice23 <mojo12984@gmail.com>
2022-03-26 19:25:09 +0100

    added helpers

---
+++ src/helpers/playableClasses.js
@@ -0,0 +1,66 @@
+//Classes
+
+export const warrior = {
+  className: "Warrior",
+  imgPath: "assets/class-icons/Warrior.png",
+};
+export const hunter = {
+  className: "Hunter",
+  imgPath: "assets/class-icons/Hunter.png",
+};
+export const deathKnight = {
+  className: "Death Knight",
+  imgPath: "assets/class-icons/Death_Knight.png",
+};
+export const mage = {
+  className: "Mage",
+  imgPath: "assets/class-icons/Mage.png",
+};
+export const priest = {
+  className: "Priest",
+  imgPath: "assets/class-icons/Priest.png",
+};
+export const monk = {
+  className: "Monk",
+  imgPath: "assets/class-icons/Monk.png",
+};
+export const rogue = {
+  className: "Rogue",
+  imgPath: "assets/class-icons/Rogue.png",
+};
+export const warlock = {
+  className: "Warlock",
+  imgPath: "assets/class-icons/Warlock.png",
+};
+export const shaman = {
+  className: "Shaman",
+  imgPath: "assets/class-icons/Shaman.png",
+};
+export const paladin = {
+  className: "Paladin",
+  imgPath: "assets/class-icons/Paladin.png",
+};
+export const druid = {
+  className: "Druid",
+  imgPath: "assets/class-icons/Druid.png",
+};
+export const demonHunter = {
+  className: "Demon Hunter",
+  imgPath: "assets/class-icons/Demon_Hunter.png",
+};
+// END of Classes
+
+export const classes = [
+  warrior,
+  hunter,
+  deathKnight,
+  mage,
+  priest,
+  monk,
+  rogue,
+  warlock,
+  shaman,
+  paladin,
+  druid,
+  demonHunter,
+];
