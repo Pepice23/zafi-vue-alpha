f43602281c5772ef5002ae505487e53a8845081a
Pepice23 <mojo12984@gmail.com>
2022-03-26 21:48:50 +0100

    Create ClassList.vue

---
+++ src/components/ClassList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row" v-for="cls in props.classList" :key="cls.className">
+    <div class="col">
+      <button
+        class="btn m-1"
+        @click="newCharacterStore.setPlayerClass(cls)"
+        :class="newCharacterStore.playerClass === cls ? 'btn-success' : ''"
+      >
+        <figure>
+          <img :src="cls.imgPath" :alt="cls.className" class="crest" />
+          <figcaption>{{ cls.className }}</figcaption>
+        </figure>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useNewCharacterStore } from "../stores/newCharacterStore";
+const props = defineProps(["classList"]);
+const newCharacterStore = useNewCharacterStore();
+</script>
+
+<style scoped>
+.crest {
+  width: 100px;
+  height: 100px;
+}
+</style>
