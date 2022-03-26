dbd00fbf2f70cb302fec4c537b4e7c9ab30b2612
Pepice23 <mojo12984@gmail.com>
2022-03-26 21:00:44 +0100

    added props to raceList

---
+++ src/components/RaceList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col" v-for="race in props.raceList" :key="race.uid">
+      <figure>
+        <img :src="race.imgPath" :alt="race.raceName" />
+        <figcaption>{{ race.raceName }}</figcaption>
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["raceList"]);
+</script>
+
+<style scoped></style>
