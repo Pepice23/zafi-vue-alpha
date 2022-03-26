3722c01ed11a5e0fa798fbecc5b4159289db3100
Pepice23 <mojo12984@gmail.com>
2022-03-26 21:10:35 +0100

    added setRace

--- src/components/RaceList.vue
+++ src/components/RaceList.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="row">
-    <div class="col" v-for="race in props.raceList" :key="race.uid">
-      <figure>
-        <img :src="race.imgPath" :alt="race.raceName" />
-        <figcaption>{{ race.raceName }}</figcaption>
-      </figure>
+  <div class="row" v-for="race in props.raceList" :key="race.uid">
+    <div class="col">
+      <button
+        class="btn m-1"
+        @click="newCharacterStore.setRace(race)"
+        :class="newCharacterStore.race === race ? 'btn-success' : ''"
+      >
+        <figure>
+          <img :src="race.imgPath" :alt="race.raceName" />
+          <figcaption>{{ race.raceName }}</figcaption>
+        </figure>
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { useNewCharacterStore } from "../stores/newCharacterStore";
 const props = defineProps(["raceList"]);
+const newCharacterStore = useNewCharacterStore();
 </script>
 
 <style scoped></style>
