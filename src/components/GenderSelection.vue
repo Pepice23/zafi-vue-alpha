0263d41dbec0087347ea049165019d714cd5e7a6
Pepice23 <mojo12984@gmail.com>
2022-03-26 20:02:11 +0100

    created GenderSelection component

---
+++ src/components/GenderSelection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <button
+        class="btn m-1"
+        @click="newCharacterStore.setGender('Male')"
+        :class="newCharacterStore.gender === 'Male' ? 'btn-success' : ''"
+      >
+        <figure>
+          <img src="src/assets/gender-icons/gender_male.png" alt="Male" />
+          <figcaption>Male</figcaption>
+        </figure>
+      </button>
+      <button
+        class="btn m-1"
+        @click="newCharacterStore.setGender('Female')"
+        :class="newCharacterStore.gender === 'Female' ? 'btn-success' : ''"
+      >
+        <figure>
+          <img src="src/assets/gender-icons/gender_female.png" alt="Female" />
+          <figcaption>Female</figcaption>
+        </figure>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useNewCharacterStore } from "../stores/newCharacterStore.js";
+
+const newCharacterStore = useNewCharacterStore();
+</script>
+
+<style scoped></style>
