f4e429eace715b023cbc85c98f7ca682d72cd730
Pepice23 <mojo12984@gmail.com>
2022-03-26 09:26:24 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="text-center container">
+    <h1 class="text-success">Zafi És Az I.N.F.</h1>
+    <button class="btn btn-primary m-1" @click="decrement">-</button>
+    <span>{{ counter }}</span>
+    <button class="btn btn-primary m-1" @click="increment">+</button>
+  </div>
+</template>
+
+<!-- Composition API -->
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+    //increment function  - increment the counter by 1
+    const increment = () => {
+      counter.value++;
+    };
+    const decrement = () => {
+      counter.value--;
+    };
+    return { counter, increment, decrement };
+  },
+};
+</script>
+
+<!-- Options API
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+  },
+};
+</script>
+-->
