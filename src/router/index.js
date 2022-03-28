f7d03bf01e3e6ee4598cc41f382eafb428f2b020
Pepice23 <mojo12984@gmail.com>
2022-03-28 10:32:23 +0200

    added character Creator to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import CharacterCreatorPage from "../views/CharacterCreatorPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/creator",
+      name: "character-creator",
+      component: CharacterCreatorPage,
     },
   ],
 });
