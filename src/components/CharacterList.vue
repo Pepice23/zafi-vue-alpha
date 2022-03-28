a70e5591125da391454709da2863b5950e6301b7
Pepice23 <mojo12984@gmail.com>
2022-03-28 17:33:02 +0200

    Create CharacterList.vue

---
+++ src/components/CharacterList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row" v-for="character in allCharacterStore.allCharacters">
+    <CharacterPanel :character="character" />
+  </div>
+</template>
+
+<script setup>
+import { useAllCharactersStore } from "../stores/allCharacterStore";
+import { onBeforeUpdate } from "vue";
+import CharacterPanel from "./CharacterPanel.vue";
+
+const allCharacterStore = useAllCharactersStore();
+allCharacterStore.getEveryCharacter();
+
+onBeforeUpdate(() => {
+  console.log("mounted");
+});
+</script>
+
+<style scoped></style>
