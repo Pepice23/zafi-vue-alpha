468c187787c17647cf4a7d8db8bc181d229377c9
Pepice23 <mojo12984@gmail.com>
2022-03-28 10:33:11 +0200

    added name, error, success to the newcharacterstore

--- src/stores/newCharacterStore.js
+++ src/stores/newCharacterStore.js
@@ -1,12 +1,16 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useNewCharacterStore = defineStore({
   id: "newCharacterStore",
   state: () => ({
+    name: "",
     faction: "",
     gender: "",
     race: {},
     playerClass: {},
+    error: {},
+    success: "",
   }),
   getters: {},
   actions: {
@@ -30,6 +34,39 @@ export const useNewCharacterStore = defineStore({
     },
     resetPlayerClass() {
       this.playerClass = {};
+    },
+    resetError() {
+      this.error = {};
+    },
+    resetSuccess() {
+      this.success = "";
+    },
+    resetName() {
+      this.name = "";
+    },
+    async checkNameAtAPI(name) {
+      try {
+        const data = await axios.get(
+          `http://127.0.0.1:8000/api/namecheck/${name}`
+        );
+        if (data.status === 200) {
+          this.name = data.data.name;
+          this.success = "Character name is available";
+        }
+      } catch (error) {
+        if (error.response === undefined) {
+          this.error = {
+            errorMessage: "The API is not available at the moment.",
+            statusCode: -1,
+          };
+        } else {
+          this.error = {
+            errorMessage: error.response.data,
+            statusCode: error.response.status,
+          };
+          this.resetName();
+        }
+      }
     },
   },
 });
