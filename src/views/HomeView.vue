68b76f03798ade002f9b7698e1e29a04132414fc
Pepice23 <mojo12984@gmail.com>
2022-03-28 17:33:25 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,15 @@
   <div class="text-center container">
     <h1 class="text-success">Zafi És Az I.N.F.</h1>
     <!--    <CharacterCreator />-->
-    <NameInput></NameInput>
+    <!--    <NameInput></NameInput>-->
+    <CharacterList />
   </div>
 </template>
 
 <!--Composition API with script setup (Recommended)-->
 <script setup>
 // import CharacterCreator from "../components/CharacterCreator.vue";
-import NameInput from "../components/NameInput.vue";
+// import NameInput from "../components/NameInput.vue";
+
+import CharacterList from "../components/CharacterList.vue";
 </script>
