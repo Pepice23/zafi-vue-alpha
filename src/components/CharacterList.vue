f040ecca9972d64644485f8601270bc2de4bcc31
Pepice23 <mojo12984@gmail.com>
2022-03-29 10:29:28 +0200

    added key prop and removed unnecessary code

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="row" v-for="character in allCharacterStore.allCharacters">
+  <div
+    class="row"
+    v-for="character in allCharacterStore.allCharacters"
+    :key="character.character_name"
+  >
     <CharacterPanel :character="character" />
   </div>
 </template>
 
 <script setup>
 import { useAllCharactersStore } from "../stores/allCharacterStore";
-import { onBeforeUpdate } from "vue";
 import CharacterPanel from "./CharacterPanel.vue";
 
 const allCharacterStore = useAllCharactersStore();
 allCharacterStore.getEveryCharacter();
-
-onBeforeUpdate(() => {
-  console.log("mounted");
-});
 </script>
 
 <style scoped></style>
