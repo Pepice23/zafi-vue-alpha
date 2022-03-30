cc67e7bfe76991dc8d40f687102a7caade132e73
Pepice23 <mojo12984@gmail.com>
2022-03-30 09:27:24 +0200

    added button to character List for creating new characters

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -6,14 +6,26 @@
   >
     <CharacterPanel :character="character" />
   </div>
+  <div class="row m-1">
+    <button class="btn btn-primary" @click="createNewCharacter">
+      Create New Character
+    </button>
+  </div>
 </template>
 
 <script setup>
 import { useAllCharactersStore } from "../stores/allCharacterStore";
 import CharacterPanel from "./CharacterPanel.vue";
+import { useRouter } from "vue-router";
 
 const allCharacterStore = useAllCharactersStore();
 allCharacterStore.getEveryCharacter();
+
+const router = useRouter();
+
+const createNewCharacter = () => {
+  router.push("/");
+};
 </script>
 
 <style scoped></style>
