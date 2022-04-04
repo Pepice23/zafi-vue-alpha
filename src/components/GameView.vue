9407a8bb08358c10ad1279cdad4300d118077083
Pepice23 <mojo12984@gmail.com>
2022-04-04 09:47:34 +0200

    Create GameView.vue

---
+++ src/components/GameView.vue
@@ -0,0 +1,20 @@
+<template>
+  GameView works!
+  {{ selectedCharacterStore }}
+  <!-- <ExtendedCharacterPanel :character="selectedCharacterStore" /> -->
+</template>
+
+<script setup>
+import { useSelectedCharacterStore } from "../stores/selectedCharacterStore";
+// import ExtendedCharacterPanel from "./ExtendedCharacterPanel.vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const selectedCharacterStore = useSelectedCharacterStore();
+const getCharacter = () => {
+  selectedCharacterStore.getSelectedCharacterFromAPI(route.params.name);
+};
+
+getCharacter();
+</script>
+
+<style scoped></style>
