bd165bcc6a1dd4f8068836ad41664ab470bfa85f
Pepice23 <mojo12984@gmail.com>
2022-05-03 11:37:13 +0200

    added buttons for the main activities and started doing the UI for them

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -1,11 +1,23 @@
 <template>
   <ExtendedCharacterPanel :character="selectedCharacterData" />
+  <div class="row">
+    <div class="col">
+      <div class="vstack gap-1">
+        <div><button class="btn btn-primary">Quest</button></div>
+        <div><button class="btn btn-primary">Dungeon</button></div>
+        <div><button class="btn btn-primary">Raid</button></div>
+      </div>
+    </div>
+    <div class="col"><DungeonUI :dungeon="DungeonData" /></div>
+  </div>
 </template>
 
 <script setup>
 import { selectedCharacterData } from "../helpers/selectedCharacterData";
+import { DungeonData } from "../helpers/DungeonData";
 // import { useSelectedCharacterStore } from "../stores/selectedCharacterStore";
 import ExtendedCharacterPanel from "./ExtendedCharacterPanel.vue";
+import DungeonUI from "./DungeonUI.vue";
 // import { useRoute } from "vue-router";
 // const route = useRoute();
 // const selectedCharacterStore = useSelectedCharacterStore();
