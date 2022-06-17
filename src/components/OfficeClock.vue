d59887b8a8a222d01b5f48fc61d9e6227df9ecdb
Pepice23 <mojo12984@gmail.com>
2022-06-17 10:58:59 +0200

    Create OfficeClock.vue

---
+++ src/components/OfficeClock.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ officeStore.time }}:00</h1>
+  <button
+    class="btn btn-primary"
+    @click="officeStore.startTimer"
+    :disabled="officeStore.timerStarted"
+  >
+    Start Day!
+  </button>
+</template>
+
+<script setup>
+import { useOfficeStore } from "../stores/officeStore.js";
+
+const officeStore = useOfficeStore();
+</script>
+
+<style scoped></style>
