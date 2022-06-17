891970db3578a830a581d6880a04a5c8f8207383
Pepice23 <mojo12984@gmail.com>
2022-06-17 11:43:38 +0200

    Create BigWorkButton.vue

---
+++ src/components/BigWorkButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn btn-primary big-work-button">WORK!!!</button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.big-work-button {
+  width: 200px;
+  height: 200px;
+}
+</style>
