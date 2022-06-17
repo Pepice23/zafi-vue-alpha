cd7d84f1ad25fa95d81796abded58b2fd0700600
Pepice23 <mojo12984@gmail.com>
2022-06-17 10:59:04 +0200

    Create OfficeView.vue

---
+++ src/views/OfficeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    Welcome to the Office! Your task is to do all he work before the clock
+    reaches 16:00
+  </h1>
+  <div class="row">
+    <OfficeClock />
+  </div>
+</template>
+
+<script setup>
+import OfficeClock from "../components/OfficeClock.vue";
+</script>
+
+<style scoped></style>
