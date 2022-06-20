2c6af63cb5d15c8af21a63be8e75c83e0036bb51
Pepice23 <mojo12984@gmail.com>
2022-06-20 19:00:39 +0200

    added in the new activities

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -1,31 +1,60 @@
 <template>
-  <ExtendedCharacterPanel :character="selectedCharacterData" />
+  <ExtendedCharacterPanel :character="selectedCharacterStore" />
   <div class="row">
     <div class="col">
       <div class="vstack gap-1">
-        <div><button class="btn btn-primary">Quest</button></div>
-        <div><button class="btn btn-primary">Dungeon</button></div>
-        <div><button class="btn btn-primary">Raid</button></div>
+        <div>
+          <button class="btn btn-primary" @click="changeActivity('quest')">
+            Quest
+          </button>
+        </div>
+        <div>
+          <button class="btn btn-primary" @click="changeActivity('dungeon')">
+            Dungeon
+          </button>
+        </div>
+        <div>
+          <button class="btn btn-primary" @click="changeActivity('raid')">
+            Raid
+          </button>
+        </div>
       </div>
     </div>
-    <div class="col"><DungeonUI :dungeon="DungeonData" /></div>
+    <div class="col" v-if="activeActivity === 'quest'">
+      <QuestUI :quest="QuestData" />
+    </div>
+    <div class="col" v-if="activeActivity === 'dungeon'">
+      <DungeonUI :dungeon="DungeonData" />
+    </div>
+    <div class="col" v-if="activeActivity === 'raid'">
+      <RaidUI :raid="RaidData" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { selectedCharacterData } from "../helpers/selectedCharacterData";
-import { DungeonData } from "../helpers/DungeonData";
-// import { useSelectedCharacterStore } from "../stores/selectedCharacterStore";
+import { DungeonData, QuestData, RaidData } from "../helpers/DungeonData";
+import { useSelectedCharacterStore } from "../stores/selectedCharacterStore";
 import ExtendedCharacterPanel from "./ExtendedCharacterPanel.vue";
 import DungeonUI from "./DungeonUI.vue";
-// import { useRoute } from "vue-router";
-// const route = useRoute();
-// const selectedCharacterStore = useSelectedCharacterStore();
-// const getCharacter = () => {
-//   selectedCharacterStore.getSelectedCharacterFromAPI(route.params.name);
-// };
+import QuestUI from "./QuestUI.vue";
+import RaidUI from "./RaidUI.vue";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const selectedCharacterStore = useSelectedCharacterStore();
+const getCharacter = () => {
+  selectedCharacterStore.getSelectedCharacterFromAPI(route.params.name);
+};
 
-// getCharacter();
+getCharacter();
+
+const activeActivity = ref("");
+
+const changeActivity = (activity) => {
+  activeActivity.value = activity;
+};
 </script>
 
 <style scoped></style>
