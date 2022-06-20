a4411b15ff9f6dc5ef9293c8deacbf644a596cf9
Pepice23 <mojo12984@gmail.com>
2022-06-20 08:19:31 +0200

    added store here as well as starting the day here makes more sense

--- src/views/OfficeView.vue
+++ src/views/OfficeView.vue
@@ -2,16 +2,46 @@
   <div class="row m-1">
     <OfficeClock />
   </div>
+  <div class="row m-1">
+    <button
+      class="btn btn-primary"
+      @click="officeStore.startDay"
+      :disabled="officeStore.timerStarted"
+    >
+      Start Day!
+    </button>
+  </div>
+  <div class="row m-1">
+    <div class="col"><h2>Work Remaining:</h2></div>
+  </div>
+  <div class="row m-1">
+    <div class="col">
+      <h2>{{ officeStore.workToBeDone }}</h2>
+    </div>
+  </div>
   <div class="row m-3">
     <div class="col">
-      <BigWorkButton></BigWorkButton>
+      <BigWorkButton />
+    </div>
+  </div>
+  <div class="row m-1">
+    <div class="col">
+      <h2>WPC/MPC: {{ officeStore.wpc }} / {{ officeStore.mpc }}</h2>
+    </div>
+  </div>
+  <div class="row m-1">
+    <div class="col">
+      <h2>WPS/MPS: {{ officeStore.wps }}/s / {{ officeStore.mps }}/s</h2>
     </div>
   </div>
 </template>
 
 <script setup>
-import OfficeClock from "../components/OfficeClock.vue";
-import BigWorkButton from "../components/BigWorkButton.vue";
+import OfficeClock from "../components/office/OfficeClock.vue";
+import BigWorkButton from "../components/office/BigWorkButton.vue";
+import { useOfficeStore } from "../stores/officeStore.js";
+
+const officeStore = useOfficeStore();
 </script>
 
 <style scoped></style>
