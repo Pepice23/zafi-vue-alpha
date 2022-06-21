fe98186c4ac9cc0bb735d75af3d11f86d0478613
Pepice23 <mojo12984@gmail.com>
2022-06-21 09:09:29 +0200

    card for the individual office upgrades

---
+++ src/components/office/OfficeUpgradeCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ upgradeName }}</h5>
+      <div v-if="upgradeDetails.timer === 0" class="m-1">
+        <h5 class="card-text">
+          {{ upgradeDetails.WPC }} WPC / {{ upgradeDetails.MPC }} MPC
+        </h5>
+      </div>
+
+      <div v-if="upgradeDetails.timer > 0" class="m-1">
+        <h5 class="card-text">
+          {{ upgradeDetails.WPS }} WPS / {{ upgradeDetails.MPS }} MPS
+        </h5>
+        <h5 class="card-text">Time: {{ upgradeDetails.timer }} sec</h5>
+      </div>
+
+      <button class="btn btn-primary">Buy Upgrade</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ upgradeName: String, upgradeDetails: Object });
+</script>
+<style scoped></style>
