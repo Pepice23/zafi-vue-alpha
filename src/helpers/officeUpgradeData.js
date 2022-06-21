897cdd11eddf21461272f38c58c6f46b9ec3625b
Pepice23 <mojo12984@gmail.com>
2022-06-21 09:10:27 +0200

    testing dummy data for the office upgrades

---
+++ src/helpers/officeUpgradeData.js
@@ -0,0 +1,18 @@
+const plus1Hand = {
+  name: "Plus 1 Hand",
+  details: { WPC: 1, MPC: 1, timer: 0 },
+};
+const pythonWorker = {
+  name: "Python Worker",
+  details: { WPS: 5, MPS: 5, timer: 10 },
+};
+const stackOverflowMagic = {
+  name: "Stack Overflow Magic",
+  details: { WPS: 10, MPS: 10, timer: 30 },
+};
+
+export const OfficeUpgradeData = {
+  plus1Hand,
+  pythonWorker,
+  stackOverflowMagic,
+};
