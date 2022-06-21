4b760a05a2c5825809a47d18c0bb754c9c55070b
Pepice23 <mojo12984@gmail.com>
2022-06-21 09:09:58 +0200

    list for all of the office upgrades

---
+++ src/components/office/OfficeUpgradeList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row" v-for="upgrade in OfficeUpgradeData" :key="upgrade.name">
+    <OfficeUpgradeCard
+      :upgrade-name="upgrade.name"
+      :upgrade-details="upgrade.details"
+    />
+  </div>
+</template>
+
+<script setup>
+import { OfficeUpgradeData } from "../../helpers/officeUpgradeData.js";
+import OfficeUpgradeCard from "./OfficeUpgradeCard.vue";
+</script>
+
+<style scoped></style>
