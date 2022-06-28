83450354468da1144d56166ac2bb2c42a5b95765
Pepice23 <mojo12984@gmail.com>
2022-06-28 08:42:45 +0200

    added in registration form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="text-center container">
     <h1 class="text-success">Zafi És Az I.N.F.</h1>
-    <NameInput></NameInput>
   </div>
+  <RegistrationForm />
 </template>
 
 <!--Composition API with script setup (Recommended)-->
 <script setup>
-import NameInput from "../components/NameInput.vue";
+import RegistrationForm from "../components/RegistrationForm.vue";
 </script>
