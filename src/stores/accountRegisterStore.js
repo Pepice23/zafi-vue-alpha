552427466f2366d907be5be2881fb38fa9ea087d
Pepice23 <mojo12984@gmail.com>
2022-07-11 10:39:27 +0200

    added checks for username and email at the API and registration as well
    @mrtnkiss After registration django prints "Broken pipe"

--- src/stores/accountRegisterStore.js
+++ src/stores/accountRegisterStore.js
@@ -1,6 +1,5 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-// import axios from "axios";
 
 export const useAccountRegisterStore = defineStore({
   id: "accountRegisterStore",
@@ -11,10 +10,25 @@ export const useAccountRegisterStore = defineStore({
     password2: "",
     error: {},
     success: "",
+    accountReady: false,
   }),
   actions: {
+    checkAccount() {
+      this.accountReady = !!(
+        this.userName &&
+        this.email &&
+        this.password1 &&
+        this.password2
+      );
+    },
     resetUserName() {
       this.userName = "";
+    },
+    resetError() {
+      this.error = {};
+    },
+    resetSuccess() {
+      this.success = "";
     },
     async checkUserNameAtAPI(name) {
       try {
@@ -22,8 +36,35 @@ export const useAccountRegisterStore = defineStore({
           `http://127.0.0.1:8000/api/usernamecheck/${name}`
         );
         if (data.status === 200) {
-          this.name = data.data.name;
+          this.userName = data.data.username;
           this.success = "Username is available";
+          this.resetError();
+        }
+      } catch (error) {
+        this.resetSuccess();
+        if (error.response === undefined) {
+          this.error = {
+            errorMessage: "The API is not available at the moment.",
+            statusCode: -1,
+          };
+        } else {
+          this.error = {
+            errorMessage: error.response.data,
+            statusCode: error.response.status,
+          };
+          this.resetUserName();
+        }
+      }
+    },
+    async checkEmailAtAPI(email) {
+      try {
+        const data = await axios.get(
+          `http://127.0.0.1:8000/api/emailcheck/${email}`
+        );
+        if (data.status === 200) {
+          this.email = data.data.email;
+          this.success = "Email is available";
+          this.resetError();
         }
       } catch (error) {
         if (error.response === undefined) {
@@ -38,6 +79,21 @@ export const useAccountRegisterStore = defineStore({
           };
           this.resetUserName();
         }
+      }
+    },
+    async registerAccountAtAPI() {
+      try {
+        await axios.post(
+          "http://127.0.0.1:8000/api/dj-rest-auth/registration/",
+          {
+            username: this.userName,
+            password1: this.password1,
+            password2: this.password2,
+            email: this.email,
+          }
+        );
+      } catch (error) {
+        console.log(error);
       }
     },
   },
