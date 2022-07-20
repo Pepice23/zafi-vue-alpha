2dc39c81cbe7f6f48ec867b2bb1d604b1a4cad27
Pepice23 <mojo12984@gmail.com>
2022-07-20 08:28:36 +0200

    created login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Login</h1>
+  <div>
+    <form @submit.prevent="accountLoginStore.loginAccountAtAPI">
+      <div class="mb-3">
+        <label for="inputUsername" class="form-label">Username:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="inputUsername"
+          v-model="accountLoginStore.userName"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="inputPassword" class="form-label">Password:</label>
+        <input
+          type="password"
+          class="form-control"
+          id="inputPassword"
+          v-model="accountLoginStore.password"
+        />
+      </div>
+      <button
+        type="submit"
+        class="btn btn-primary"
+        :disabled="
+          !accountLoginStore.userName.length < 3 &&
+          accountLoginStore.password.length < 8
+        "
+      >
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { useAccountLoginStore } from "../stores/accountLoginStore";
+const accountLoginStore = useAccountLoginStore();
+</script>
+
+<style scoped></style>
